0f676051c5b11cb646559fefeb4a60142e9a86dc
Lucas G. Cordeiro <lucasgon192@gmail.com>
2023-08-01 17:38:05 -0400

    feat: post /trade send a new trade offer

--- src/trade/trade.controller.js
+++ src/trade/trade.controller.js
@@ -1,10 +1,78 @@
 const { database } = require('../database/knex');
 
-function sendTradeOffer(req, res) {
+async function sendTradeOffer(req, res) {
+  const proposer = req.body.payload.playerId;
+  const {
+    acceptor,
+    offeredItems,
+    requestedItems,
+  } = req.body;
+
+  // Verify if any of the required data is undefined
+  const isAnyFieldUndefined = !proposer || !acceptor || !offeredItems || !requestedItems;
+  if (isAnyFieldUndefined) {
+    return res.status(400).send({
+      message: 'The fields acceptor, offeredItems and requestedItems are required',
+    });
+  }
+
+  // Verify if offeredItems and requestedItems are arrays
+  const tradeItemsAreArray = Array.isArray(offeredItems) && Array.isArray(requestedItems);
+  if (!tradeItemsAreArray) {
+    return res.status(400).send({
+      message: 'offeredItems and requestedItems must be an array of items uuid',
+    });
+  }
+
+  // Validate if each player owns the specified items
   try {
-    res.send({ message: 'Sending a trade offer' });
+    const proposerOwnedItems = await database.trade.getItemsFromPlayer(proposer, offeredItems);
+    const proposerOwnsAllTheOfferedItems = offeredItems.length === proposerOwnedItems.length;
+    if (!proposerOwnsAllTheOfferedItems) {
+      return res.status(400).send({
+        message: 'You cannot offer items that you don\'t own',
+      });
+    }
+
+    const acceptorOwnedItems = await database.trade.getItemsFromPlayer(acceptor, requestedItems);
+    const acceptorOwnsAllTheRequestedItems = requestedItems.length === acceptorOwnedItems.length;
+    if (!acceptorOwnsAllTheRequestedItems) {
+      return res.status(400).send({
+        message: 'You cannot request items that the acceptor doesn\'t own',
+      });
+    }
   } catch (err) {
-    console.error('Error while sending trade offer', err.message);
+    return res.status(400).send({
+      message: 'Check and verify the data sent',
+      error: err,
+    });
+  }
+
+  // Verify if every item is available for new trades
+  try {
+    const allTradingItems = [...offeredItems, ...requestedItems];
+    const unavailableItems = await database.trade.getUntradeableItems(allTradingItems);
+    if (unavailableItems.length > 0) {
+      return res.status(409).send({
+        message: 'There are items on pending trades',
+        items: unavailableItems,
+      });
+    }
+  } catch (err) {
+    return res.status(500).send({
+      message: 'Internal Server Error :(',
+      error: err,
+    });
+  }
+
+  try {
+    await database.trade.createTrade(proposer, acceptor, offeredItems, requestedItems);
+    return res.status(201).end();
+  } catch (err) {
+    return res.status(500).send({
+      message: 'Internal Server Error :(',
+      error: err,
+    });
   }
 }
 
