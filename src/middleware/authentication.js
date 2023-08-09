14ac9c31093ca31d094bfb2f5eb67e929bbdda7c
Falcão <gabriel.p.falcao50@gmail.com>
2023-08-08 22:25:49 -0400

    feat: create authentication middleware

---
+++ src/middleware/authentication.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const { AuthenticationError } = require('../error/AuthenticationError');
+
+function authenticateToken(req, res, next) {
+  if (req.headers.cookie == null) {
+    res.send(new AuthenticationError());
+  }
+
+  const authHeader = req.headers.cookie;
+  const token = authHeader && authHeader.split('=')[1];
+  console.log(token);
+
+  if (token == null) {
+    res.send(new AuthenticationError());
+  }
+
+  return jwt.verify(token, process.env.SECRET, (err, player) => {
+    if (err) {
+      res.send(new AuthenticationError());
+    }
+
+    req.player = player;
+    console.log(player);
+    return next();
+  });
+}
+
+module.exports = authenticateToken;
