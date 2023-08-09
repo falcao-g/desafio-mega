e673490e096e91ba2312cda1e13b5dc358a5aff7
Falcão <gabriel.p.falcao50@gmail.com>
2023-08-08 23:06:43 -0400

    feat: integrate authentication in trade routes

--- src/trade/trade.route.js
+++ src/trade/trade.route.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const authenticateToken = require('../middleware/authentication');
 
 const router = express.Router();
 const tradeService = require('./trade.service');
 
-router.get('/offer', tradeService.listAllTradeOffersFromPlayer);
-router.post('/', tradeService.sendTradeOffer);
-router.post('/offer/:tradeId/accept', tradeService.acceptTradeOffer);
-router.post('/offer/:tradeId/decline', tradeService.declineTradeOffer);
-router.delete('/offer/:tradeId', tradeService.cancelTradeOffer);
+router.get('/offer', authenticateToken, tradeService.listAllTradeOffersFromPlayer);
+router.post('/', authenticateToken, tradeService.sendTradeOffer);
+router.post('/offer/:tradeId/accept', authenticateToken, tradeService.acceptTradeOffer);
+router.post('/offer/:tradeId/decline', authenticateToken, tradeService.declineTradeOffer);
+router.delete('/offer/:tradeId', authenticateToken, tradeService.cancelTradeOffer);
 
 module.exports = router;
