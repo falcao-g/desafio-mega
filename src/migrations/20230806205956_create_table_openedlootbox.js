6ef5494c5b8c6845bf746810408c17ff8af2c444
Pedro Henrique Janini <pedro.janini@gmail.com>
2023-08-10 21:53:34 -0400

    fix: lootbox migrations

--- src/migrations/20230806205956_create_table_openedlootbox.js
+++ src/migrations/20230806205956_create_table_openedlootbox.js
@@ -2,14 +2,12 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = (knex) => {
-  knex.schema.createTable('openedlootbox', (table) => {
-    table.uuid('uuid').primary();
-    table.uuid('lootbox').notNullable().references('uuid').inTable('lootbox');
-    table.uuid('player').notNullable().references('uuid').inTable('player');
-    table.timestamp('openedAt', { useTz: true }).notNullable().defaultTo(knex.fn.now());
-  });
-};
+exports.up = (knex) => knex.schema.createTable('openedlootbox', (table) => {
+  table.uuid('uuid').primary();
+  table.uuid('lootbox').notNullable().references('uuid').inTable('lootbox');
+  table.uuid('player').notNullable().references('uuid').inTable('player');
+  table.timestamp('openedAt', { useTz: true }).notNullable().defaultTo(knex.fn.now());
+});
 
 /**
  * @param { import("knex").Knex } knex
