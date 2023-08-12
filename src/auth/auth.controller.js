e5960be379bc7e45e57369cb0d8ecc5ddcdd12eb
Falcão <gabriel.p.falcao50@gmail.com>
2023-08-11 23:31:18 -0400

    fix: add missing field checks

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -14,18 +14,24 @@ async function encryptPassword(password) {
 }
 
 async function registerPlayer(name, login, password) {
+  if (!name) throw new ValidationError('Name is required');
+  if (!login) throw new ValidationError('Login is required');
+  if (!password) throw new ValidationError('Password is required');
+  await checkIfPlayerAlreadyExists(login);
   const encryptedPassword = await encryptPassword(password);
   const message = await database.auth.registerPlayer(name, login, encryptedPassword);
   return message;
 }
 
 async function getPlayerByLogin(login) {
+  if (!login) throw new ValidationError('Login is required');
   const player = await database.auth.findOne(login);
   if (!player) throw new ValidationError('Unknown player');
   return player;
 }
 
 async function checkPassword(password, encryptedPassword) {
+  if (!password) throw new ValidationError('Password is required');
   const isPasswordValid = await bcrypt.compare(password, encryptedPassword);
   if (!isPasswordValid) throw new ValidationError('Invalid password');
   return isPasswordValid;
